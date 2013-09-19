42020a6040098811b38e4488384ef47e1360d9a6
Sébastien Crozet <developer@crozet.re>
2013-09-19 15:59:10 +0200

    Merge example files to make them easier to read for bjz/glfw-rs#33.

--- src/examples/shared-state-callback-tls-with-usr-tls/main.rs
+++ src/examples/shared-state-callback-tls-with-usr-tls/main.rs
@@ -1,5 +1,5 @@
 
-// Copyright 2013 The GLFW-RS Developers. For a full listing of the authors,
+/ Copyright 2013 The GLFW-RS Developers. For a full listing of the authors,
 // refer to the AUTHORS file at the top-level directory of this distribution.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,9 +17,9 @@
 //! This example shows how shared objects can be accessed from callbacks
 //!
 //! This example should be seen as two different projects:
-//!     * `middleware.rs` is written as if we are a middleware library writer. Typically, such
+//!     * the first part is written as if we are the user of the middleware library.
+//!     * the second part is written as if we are a middleware library writer.  Typically, such
 //!     middleware library can be a graphics engine.
-//!     * `main.rs` (this file) is written as if we are the user of the middleware library.
 //!
 //! The goal is to show how the middleware library writer can write its event handling in a
 //! flexible way for the user. That is, he must both use glfw callbacks to update the scene (here
@@ -30,15 +30,13 @@
 
 extern mod glfw;
 use std::local_data;
-use middleware::Engine;
-use middleware::State;
 
-mod middleware;
-
-// since the library callbacks ar ~fn, we have to put the counter on TLS.
 // The value could be an uint instead of @mut uint. But doing so makes it really painful to use.
 static counter_tls_key: local_data::Key<@mut uint> = &local_data::Key;
 
+/*
+ * User app.
+ */
 #[start]
 fn start(argc: int, argv: **u8, crate_map: *u8) -> int {
     std::rt::start_on_main_thread(argc, argv, crate_map, main)
@@ -72,6 +70,110 @@ fn main() {
                 let c = counter.unwrap();
                 println(c.to_str());
             }
+        }
+    }
+}
+
+/*
+ * Engine
+ */
+static tls_key: local_data::Key<@mut State> = &local_data::Key;
+
+pub struct Engine {
+    window: glfw::Window
+}
+
+impl Engine {
+    pub fn spawn(callback: ~fn(&mut Engine)) {
+        do glfw::set_error_callback |_, msg| {
+            println!("GLFW Error: {:s}", msg);
+        }
+
+        do glfw::start {
+            let window = glfw::Window::create(300, 300, "Move cursor in window", glfw::Windowed).unwrap();
+            let mut engine = Engine {window: window };
+            State::init(0f, 0f);
+
+            do engine.window.set_cursor_pos_callback |_, x, y| {
+                State::handle_mouse(x, y);
+            }
+
+            do engine.window.set_key_callback |win, key, _, action, _mods| {
+                if action == glfw::PRESS && key == glfw::KEY_ESCAPE {
+                    win.set_should_close(true);
+                }
+                else {
+                    State::handle_keyboard();
+                }
+            }
+
+            engine.window.make_context_current();
+
+            callback(&mut engine);
+        }
+    }
+
+    pub fn render_loop(&mut self, callback: &fn(&mut Engine)) {
+        while !self.window.should_close() {
+            glfw::poll_events();
+            let (x, y) = State::get_pos();
+            callback(self);
+            println("Cursor pos: " + x.to_str() + ", " + y.to_str());
+        }
+    }
+}
+
+struct State {
+    priv pos: (float, float),
+
+    // callbacks are owned this time
+    priv usr_key_handler:   ~fn(),
+    priv usr_mouse_handler: ~fn()
+}
+
+impl State {
+    pub fn init(x: float, y: float) {
+        local_data::set(
+            tls_key,
+            @mut State { pos: (x, y), usr_key_handler: || { }, usr_mouse_handler: || { } }
+        );
+    }
+
+    pub fn handle_mouse(x: float, y: float) {
+        do local_data::get(tls_key) |opt| {
+            let state = opt.expect("Task-local state not initialized.");
+            state.pos = (x, y);
+            (state.usr_mouse_handler)();
+        }
+    }
+
+    pub fn handle_keyboard() {
+        do local_data::get(tls_key) |opt| {
+            (opt.expect("Task-local state not initialized.").usr_key_handler)();
+        }
+    }
+
+    pub fn get_pos() -> (float, float) {
+        do local_data::get(tls_key) |opt| {
+            opt.expect("Task-local state not initialized.").pos
+        }
+    }
+
+    pub fn set_key_event_callback(callback: ~fn()) {
+        do local_data::get(tls_key) |opt| {
+            // XXX: "Cannot move out of captured outer variable"
+            // Using a ~fn user defined callback does not seem even possible in combination with
+            // TLS…
+            opt.expect("Task-local state not initialized.").usr_key_handler = callback;
+        }
+    }
+
+    pub fn set_mouse_event_callback(callback: ~fn()) {
+        do local_data::get(tls_key) |opt| {
+            // XXX: "Cannot move out of captured outer variable"
+            // Using a ~fn user defined callback does not seem even possible in combination with
+            // TLS…
+            opt.expect("Task-local state not initialized.").usr_mouse_handler = callback;
         }
     }
 }
--- src/examples/shared-state-callback-tls/main.rs
+++ src/examples/shared-state-callback-tls/main.rs
@@ -17,9 +17,9 @@
 //! This example shows how shared objects can be accessed from callbacks
 //!
 //! This example should be seen as two different projects:
-//!     * `middleware.rs` is written as if we are a middleware library writer.  Typically, such
+//!     * the first part is written as if we are the user of the middleware library.
+//!     * the second part is written as if we are a middleware library writer. Typically, such
 //!     middleware library can be a graphics engine.
-//!     * `main.rs` (this file) is written as if we are the user of the middleware library.
 //!
 //! The goal is to show how the middleware library writer can write its event handling in a
 //! flexible way for the user. That is, he must both use glfw callbacks to update the scene (here
@@ -29,11 +29,12 @@
 //! events and display it on its main loop.
 
 extern mod glfw;
-use middleware::Engine;
-use middleware::State;
 
-mod middleware;
+use std::local_data;
 
+/*
+ * User app.
+ */
 #[start]
 fn start(argc: int, argv: **u8, crate_map: *u8) -> int {
     std::rt::start_on_main_thread(argc, argv, crate_map, main)
@@ -56,6 +57,110 @@ fn main() {
         // start the main loop
         do engine.render_loop |_| {
             println(counter.to_str());
+        }
+    }
+}
+
+/*
+ * Engine
+ */
+static tls_key: local_data::Key<@mut State> = &local_data::Key;
+
+pub struct Engine {
+    window: glfw::Window
+}
+
+impl Engine {
+    pub fn spawn(callback: ~fn(&mut Engine)) {
+        do glfw::set_error_callback |_, msg| {
+            println!("GLFW Error: {:s}", msg);
+        }
+
+        do glfw::start {
+            let window = glfw::Window::create(300, 300, "Move cursor in window", glfw::Windowed).unwrap();
+            let mut engine = Engine {window: window };
+            State::init(0f, 0f);
+
+            do engine.window.set_cursor_pos_callback |_, x, y| {
+                State::handle_mouse(x, y);
+            }
+
+            do engine.window.set_key_callback |win, key, _, action, _mods| {
+                if action == glfw::PRESS && key == glfw::KEY_ESCAPE {
+                    win.set_should_close(true);
+                }
+                else {
+                    State::handle_keyboard();
+                }
+            }
+
+            engine.window.make_context_current();
+
+            callback(&mut engine);
+        }
+    }
+
+    pub fn render_loop(&mut self, callback: &fn(&mut Engine)) {
+        while !self.window.should_close() {
+            glfw::poll_events();
+            let (x, y) = State::get_pos();
+            callback(self);
+            println("Cursor pos: " + x.to_str() + ", " + y.to_str());
+        }
+    }
+}
+
+struct State {
+    priv pos: (float, float),
+
+    // Those two callbacks must be @ and must be in the ’State’ to make them callbable indirectly
+    // by glfw callbacks.
+    //
+    // Note that those callbacks might be really useless/hard to use if they dont give the user
+    // access to the Engine: @fn(&mut Engine).
+    // But Engine, is not on TLS, so this is not possible! Anyway, assume the user does not need
+    // the Engine to proceed (or has its own @mut Engine captured by its callback).
+    priv usr_key_handler:   @fn(),
+    priv usr_mouse_handler: @fn()
+}
+
+impl State {
+    pub fn init(x: float, y: float) {
+        local_data::set(
+            tls_key,
+            @mut State { pos: (x, y), usr_key_handler: || { }, usr_mouse_handler: || { } }
+        );
+    }
+
+    pub fn handle_mouse(x: float, y: float) {
+        do local_data::get(tls_key) |opt| {
+            let state = opt.expect("Task-local state not initialized.");
+            state.pos = (x, y);
+            (state.usr_mouse_handler)();
+        }
+    }
+
+    pub fn handle_keyboard() {
+        do local_data::get(tls_key) |opt| {
+            (opt.expect("Task-local state not initialized.").usr_key_handler)();
+        }
+    }
+
+    pub fn get_pos() -> (float, float) {
+        do local_data::get(tls_key) |opt| {
+            opt.expect("Task-local state not initialized.").pos
+        }
+    }
+
+    pub fn set_key_event_callback(callback: @fn()) {
+        do local_data::get(tls_key) |opt| {
+            opt.expect("Task-local state not initialized.").usr_key_handler = callback;
+        }
+    }
+
+    pub fn set_mouse_event_callback(callback: @fn()) {
+        do local_data::get(tls_key) |opt| {
+            opt.expect("Task-local state not initialized.").usr_mouse_handler = callback;
         }
     }
 }
